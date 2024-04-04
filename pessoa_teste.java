986f6f695dfd3400a21fc996c4128a2cd8d4d8d6
Ocante António <63821194+Ocante@users.noreply.github.com>
2024-04-04 17:39:04 -0300

    pequenas mudanças

--- pessoa_teste.java
+++ pessoa_teste.java
@@ -7,7 +7,7 @@ public class Pessoa {
     private String sobrenome;
     private int idade;
     private LocalDate dataNascimento; // Adicione um campo para a data de nascimento
-
+	//agora deu certo
     public int getIdade() {
         return idade;
     }
