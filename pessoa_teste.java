6aaa7d72399571664190a017e329c4f0ff278385
Ocante António <63821194+Ocante@users.noreply.github.com>
2024-04-04 17:30:19 -0300

    Enviando um arquio de teste para repositório remoto

---
+++ pessoa_teste.java
@@ -0,0 +1,43 @@
+package curso.java;
+
+import java.time.LocalDate; // Importe a classe LocalDate para lidar com datas
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private LocalDate dataNascimento; // Adicione um campo para a data de nascimento
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
+
